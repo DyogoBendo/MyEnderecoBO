9e921c3200b7d813425f0320994211d217d32226
dyogo.bendo <dyogoromagnabendo@gmail.com>
2025-12-05 20:06:57 -0300

    feat: add endereco especifico

---
+++ src/main/java/unioeste/geral/endereco/bo/EnderecoEspecifico.java
@@ -0,0 +1,37 @@
+package unioeste.geral.endereco.bo;
+
+public class EnderecoEspecifico {
+    private Endereco endereco;
+    private String complemento;
+    private Long nroCasa;
+
+    public EnderecoEspecifico(Endereco endereco, String complemento, Long nroCasa) {
+        this.endereco = endereco;
+        this.complemento = complemento;
+        this.nroCasa = nroCasa;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public Long getNroCasa() {
+        return nroCasa;
+    }
+
+    public void setNroCasa(Long nroCasa) {
+        this.nroCasa = nroCasa;
+    }
+}
